c2ed2e4121991c82ff6922c39b272714320851dd
pritiranjan1605 <136108507+pritiranjan1605@users.noreply.github.com>
2023-11-29 17:45:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Priti Ranjan Samal",
+  title: "Backend Web Designer ",
+  email: "pritiranjansamal868@gmail.com",
+  gitHub: "https://github.com/pritiranjan1605",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/priti-ranjan-samal-5b3643224/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
